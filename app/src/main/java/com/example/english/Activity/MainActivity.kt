9da089e2bcbc2fc9ac4e381535d73656b9ba6df1
ngoctruongbui2003 <ngoctruongbui2003@gmail.com>
2023-12-09 18:31:22 +0700

    fragment UI

--- app/src/main/java/com/example/english/Activity/MainActivity.kt
+++ app/src/main/java/com/example/english/Activity/MainActivity.kt
@@ -2,11 +2,46 @@ package com.example.english.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.english.Fragment.HomeFragment
+import com.example.english.Fragment.LibraryFragment
+import com.example.english.Fragment.ProfileFragment
+import com.example.english.Fragment.SearchFragment
 import com.example.english.R
+import com.example.english.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val homeFragment: HomeFragment = HomeFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.background = null
+
+        binding.bottomNavigationView.setOnItemSelectedListener {item ->
+            when(item.itemId) {
+                R.id.navigation_home -> replaceFragment(HomeFragment())
+                R.id.navigation_search -> replaceFragment(SearchFragment())
+                R.id.navigation_add -> replaceFragment(HomeFragment())
+                R.id.navigation_library -> replaceFragment(LibraryFragment())
+                R.id.navigation_profile -> replaceFragment(ProfileFragment())
+            }
+
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
